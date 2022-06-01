de6c262028e38f514627eca0843ff7096ee7caca
Dayton <daytonbraison@gmail.com>
2022-05-31 21:28:41 -0500

    Added more code

--- python_pandas1.py
+++ python_pandas1.py
@@ -3,20 +3,35 @@ import pandas
 car_data = pandas.read_csv("car_data.csv")
 weather_data = pandas.read_csv("weather_data.csv")
 
+print(car_data)
+print('\n')
+print(weather_data)
+print('\n')
+
+# Convert to dictionaries
 car_data_dict = car_data.to_dict()
 weather_data_dict = weather_data.to_dict()
 
 print(car_data_dict)
 print('\n')
 print(weather_data_dict)
+print('\n')
 
-print(car_data['Car Model'].to_list())
+# Convert to lists
+print(car_data['Car Make'].to_list())
+print('\n')
+print(weather_data['day'].to_list())
+print('\n')
 
-# Create a dataframe from scratch
-data_dict = {
-    "students": ['Amy', 'James', 'Angela'],
-    "scores": [80, 85, 90]
+# Create a dataframe
+some_data = {
+    "athletes": ['Tom Brady', 'Kobe Bryant', 'Steph Curry'],
+    "teams": ['Tampa Bay Buccaneers', 'LA Lakers', 'Golden State Warriors']
 }
 
-my_df = pandas.DataFrame(data_dict)
+my_df = pandas.DataFrame(some_data)
 print(my_df)
+print('\n')
+
+# Creates a new csv file with your provided data
+my_df.to_csv("athlete_data.csv")
